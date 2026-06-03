a85af7c67b85ba09ad622e4ced28cc887d9e9cfc
MJGrauVi <mariajosegrauvidal@gmail.com>
2026-06-03 20:42:12 +0200

    elimino elementos no usados

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/inicio', function(){
-    return view('inicio');
-});
-Route::get('/suma', function(){
-    return view('suma');
-});
-Route::post('/suma', function(Request $request){
-
-    $num1 = $request->input('num1');
-    $num2 = $request->input('num2');
-    $resultado = $num1 + $num2;
-   /* echo "La suma de $num1 + $num2 es $resultado";*/
-    return view('suma',['resul'=> $resultado]);
-});
-
